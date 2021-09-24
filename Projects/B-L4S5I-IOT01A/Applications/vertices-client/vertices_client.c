19d4d1d5a6fa368269d21bbe76711a0eb429a947
Cyril Fougeray <cyril.fougeray@gmail.com>
2021-09-24 10:57:04 +0200

    Now linking secret key into image - do not use in production

--- Projects/B-L4S5I-IOT01A/Applications/vertices-client/vertices_client.c
+++ Projects/B-L4S5I-IOT01A/Applications/vertices-client/vertices_client.c
@@ -5,10 +5,25 @@
 #include <stdio.h>
 #include <cmsis_os.h>
 #include <vertices.h>
+#include <string.h>
 #include "vertices_client.h"
 
 static ret_code_t
 vertices_evt_handler(vtc_evt_t *evt);
+
+/***
+ * The binary data is taken from a 64-byte binary file encapsulating the private and public keys
+ * in binary format.
+ * It has been generated using the Unix example available in the Vertices SDK repo, using the \c -n flag:
+ * $ ./unix_example -n
+ * You can then copy \c private_key.bin and \c public_b32.txt into the example's \c main directory
+ * /!\ this is not a safe way to handle keys in production.
+ */
+extern const uint8_t signature_keys_start[] asm("_binary_private_key_bin_start");
+extern const uint8_t signature_key_end[] asm("_binary_private_key_bin_end");
+
+extern const uint8_t account_address_b32_start[] asm("_binary_public_b32_txt_start");
+extern const uint8_t account_address_b32_end[] asm("_binary_public_b32_txt_end");
 
 static provider_info_t providers =
     {
@@ -17,6 +32,20 @@ static provider_info_t providers =
         .header = (char *) SERVER_TOKEN_HEADER,
         .cert_pem = SERVER_CA
     };
+
+/// We store anything related to the account into the below structure
+/// The private key is used outside of the Vertices library:
+///    you don't have to pass the private key to the SDK as signing is done outside
+typedef struct
+{
+    unsigned char private_key[ADDRESS_LENGTH];  //!< 32-bytes private key
+    account_info_t *vtc_account;               //!< pointer to Vertices account data
+} account_t;
+
+// Alice's account is used to send data, keys will be retrived from config/key_files.txt
+static account_t alice_account = {.private_key = {0}, .vtc_account = NULL};
+// Bob is receiving the money 😎
+static account_t bob_account = {.private_key = {0}, .vtc_account = NULL};
 
 static ret_code_t
 vertices_evt_handler(vtc_evt_t *evt)
@@ -28,15 +57,23 @@ vertices_evt_handler(vtc_evt_t *evt)
     return err_code;
 }
 
-/**
- * @brief  Require Root Certificate for TLS.
- * @param  None
- * @retval Always true
- */
-bool
-app_needs_root_ca(void)
+static void
+load_existing_account()
 {
-    return true;
+    char public_b32[PUBLIC_B32_STR_MAX_LENGTH] = {0};
+
+    // Account address length (remove 1 char: `\n`)
+    size_t len = account_address_b32_end - account_address_b32_start - 1;
+
+    // copy keys from Flash
+    memcpy(alice_account.private_key, signature_keys_start, ADDRESS_LENGTH);
+    memcpy(public_b32, account_address_b32_start, len);
+
+    // let's compute the Algorand public address
+    ret_code_t err_code = vertices_account_new_from_b32(public_b32, &alice_account.vtc_account);
+    VTC_ASSERT(err_code);
+
+    printf("💳 Alice's account %s", alice_account.vtc_account->public_b32);
 }
 
 static vertex_t m_vertex = {
@@ -77,7 +114,7 @@ vertices_wallet_run(void const *arg)
            version.minor,
            version.patch);
 
-
+    load_existing_account();
 
     while (1)
     {
